d9984e24215cce96927e7fc752617b7c41e24f6f
Ezequiel A. <ezargon@gmail.com>
2017-09-11 13:50:50 +0200

    segunda prueba

--- configuration.php
+++ configuration.php
@@ -14,7 +14,7 @@ class JConfig {
 	public $dbtype = 'mysqli';
 	public $host = 'localhost';
 	public $user = 'root';
-	public $password = '';
+	public $password = 'root';
 	public $db = 'cursos';
 	public $dbprefix = 'cur_';
 	public $live_site = '';
