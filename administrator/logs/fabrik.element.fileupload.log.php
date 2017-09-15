6ac3a45cfd49415fc73291054971ad08bcb74aea
Ezequiel A. <ezargon@gmail.com>
2017-09-15 12:38:30 +0200

    Nueva version de BBDD

---
+++ administrator/logs/fabrik.element.fileupload.log.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-09-15 10:17:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-09-15T10:17:17+00:00	WARNING 127.0.0.1	com_fabrik.element.fileupload	Delete files: D:\Dropbox\Source Code\tolteca\images\files\undefined , D:\Dropbox\Source Code\tolteca\images\stories\thumbs\undefined., D:\Dropbox\Source Code\tolteca\images\stories\crop\undefined; user = 1
+2017-09-15T10:17:17+00:00	WARNING 127.0.0.1	com_fabrik.element.fileupload	Delete join image entry: 
+DELETE 
+FROM `cursos_repeat_documentacion`
+WHERE `id` = '3'; user = 1
