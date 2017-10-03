3659e7fdb17b85d0b28dc28ffb70186bffd71d98
Ezequiel A. <ezargon@gmail.com>
2017-10-03 10:28:47 +0200

    Actualizacion Joomla 3.8 Y creación de tabla PRogramas

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-09-29 11:23:03 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-09-29T11:23:03+00:00	INFO 127.0.0.1	update	Actualización inciciadoa por el usuario Ezequiel (1). La versión antigua es la 3.7.5.
+2017-09-29T11:23:09+00:00	INFO 127.0.0.1	update	Descargando el archivo desde https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.0-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20170929%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20170929T112453Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=0e218dc5d437f407280858030a3a9c84a1a85f062918d498796624caeb97eea7.
+2017-09-29T11:23:21+00:00	INFO 127.0.0.1	update	El archivoe Joomla_3.8.0-Stable-Update_Package.zip se ha descargado.
+2017-09-29T11:23:21+00:00	INFO 127.0.0.1	update	Iniciando la instalación de la versión nueva.
+2017-09-29T11:23:48+00:00	INFO 127.0.0.1	update	Finalizando la instalación.
+2017-09-29T11:23:48+00:00	INFO 127.0.0.1	update	Consulta ejecutada desde el archivo 3.8.0-2017-07-28. Texto de la consulta: ALTER TABLE `#__fields_groups` ADD COLUMN `params` TEXT  NOT NULL  AFTER `orderi.
+2017-09-29T11:23:48+00:00	INFO 127.0.0.1	update	Consulta ejecutada desde el archivo 3.8.0-2017-07-31. Texto de la consulta: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2017-09-29T11:23:48+00:00	INFO 127.0.0.1	update	Borrando los archivos y carpetas que han sido eliminados.
+2017-09-29T11:23:50+00:00	INFO 127.0.0.1	update	Limpieza posterior a la instalación.
+2017-09-29T11:23:50+00:00	INFO 127.0.0.1	update	La actualización a la versión 3.8.0 se ha completado.
