11c9e83c50dd71a12b9f5317053a64069c8ab698
Ezequiel A. <ezargon@gmail.com>
2017-10-06 10:26:38 +0200

    Actualización Joomla 3.8.1

--- administrator/logs/joomla_update.php
+++ administrator/logs/joomla_update.php
@@ -14,3 +14,13 @@
 2017-09-29T11:23:48+00:00	INFO 127.0.0.1	update	Borrando los archivos y carpetas que han sido eliminados.
 2017-09-29T11:23:50+00:00	INFO 127.0.0.1	update	Limpieza posterior a la instalación.
 2017-09-29T11:23:50+00:00	INFO 127.0.0.1	update	La actualización a la versión 3.8.0 se ha completado.
+2017-10-06T08:23:46+00:00	INFO 127.0.0.1	update	Actualización inciciadoa por el usuario Ezequiel (1). La versión antigua es la 3.8.0.
+2017-10-06T08:23:50+00:00	INFO 127.0.0.1	update	Descargando el archivo desde https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.1-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20171006%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171006T082541Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=391dddecb0a0f9346f80628ecd753fd58a45f5a47b72fced73039872c78a027a.
+2017-10-06T08:24:03+00:00	INFO 127.0.0.1	update	El archivoe Joomla_3.8.1-Stable-Update_Package.zip se ha descargado.
+2017-10-06T08:24:03+00:00	INFO 127.0.0.1	update	Iniciando la instalación de la versión nueva.
+2017-10-06T08:24:50+00:00	INFO 127.0.0.1	update	Finalizando la instalación.
+2017-10-06T08:24:51+00:00	INFO 127.0.0.1	update	Consulta ejecutada desde el archivo 3.8.0-2017-07-28. Texto de la consulta: ALTER TABLE `#__fields_groups` ADD COLUMN `params` TEXT  NOT NULL  AFTER `orderi.
+2017-10-06T08:24:51+00:00	INFO 127.0.0.1	update	Consulta ejecutada desde el archivo 3.8.0-2017-07-31. Texto de la consulta: INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem.
+2017-10-06T08:24:51+00:00	INFO 127.0.0.1	update	Borrando los archivos y carpetas que han sido eliminados.
+2017-10-06T08:24:52+00:00	INFO 127.0.0.1	update	Limpieza posterior a la instalación.
+2017-10-06T08:24:52+00:00	INFO 127.0.0.1	update	La actualización a la versión 3.8.1 se ha completado.
